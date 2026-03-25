7d4dc354515d53edb576064732799b31b3469b31
Niels vaandering <nvaandering1@outlook.com>
2026-03-25 21:52:39 +0100

    Part B

--- Homework/ODE/main.cpp
+++ Homework/ODE/main.cpp
@@ -28,6 +28,7 @@ int main(){
     results.push_back(yt[0]);
     results.push_back(yt[1]);
     //std::cout << "# t   u' = v    v'= -u\n";
+    std::cout<<"#Testing my ODE solver on the test function u'' = -u\n";
     results.print();
 
     // Solving the  scipy.integrate.odeint manual:
@@ -53,6 +54,50 @@ int main(){
 
     std::cout<< "\n#Datapoints for the example from scipy odeint: oscillator with friction\n#For me, the plots looks very similar\n";
     example_plotting.print();
+
+
+    //Part B: Relativistic precession of planetary orbit
+    double epsilon = 0.0;
+    auto h = [](double epsilon) {
+    return [epsilon](double t, pp::vector y){
+    (void)t; // using t without using it to prefent that error
+    pp::vector components(2); 
+    components[0] = y[1];    
+    components[1] = 1 - y[0] + epsilon * y[0] * y[0];   
+    return components;
+    };
+    };
+
+    pp::vector precession1_initial {1.0, 0.0};
+    pp::vector precession2_initial {1.0, -0.5};
+
+    //B.1:
+    auto [B1, precession1] = driver(h(epsilon),0,2e9,precession1_initial);
+    //B.2:
+    auto [B2, precession2] = driver(h(epsilon),0,30,precession2_initial);
+    epsilon = 0.01;
+    auto [B3, precession3] = driver(h(epsilon),0,30,precession2_initial);
+
+    std::cout<<"\n#Part B: question 1. ε=0 and initial conditions u(0)=1, u'(0)=0 : this should give a Newtonian circular motion.\n";
+    pp::matrix Out_B1;
+    Out_B1.push_back(B1);
+    Out_B1.push_back(precession1.transpose()[0]); 
+    Out_B1.push_back(precession1.transpose()[1]); 
+    Out_B1.print();
+    std::cout<<"\n#Part B: question 2. ε=0 and initial conditions u(0)=1, u'(0)≈-0.5 : this should give a Newtonian elliptical motion.\n";
+    pp::matrix Out_B2;
+    Out_B2.push_back(B2);
+    Out_B2.push_back(precession2.transpose()[0]); 
+    Out_B2.push_back(precession2.transpose()[1]); 
+    Out_B2.print();
+    std::cout<<"\n#Part B: question 3. ε=0.01 and initial conditions u(0)=1, u'(0)≈-0.5 : his should illustrate the relativistic precession of a planetary orbit.\n";
+    pp::matrix Out_B3;
+    Out_B3.push_back(B3);
+    Out_B3.push_back(precession3.transpose()[0]); 
+    Out_B3.push_back(precession3.transpose()[1]); 
+    Out_B3.print();
+
+
 
     return 0;
 }
